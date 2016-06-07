2a97ec25a6b61f7c859b2bc3a10e3132334d187d
Matej Ramuta <matej.ramuta@gmail.com>
2016-06-07 11:24:07 +0200

    secret template updated

--- secret-template.py
+++ secret-template.py
@@ -1,5 +1,6 @@
 _PAGE_ACCESS_TOKEN = ""
 _VERIFY_TOKEN = ""
+_API_AI_CLIENT_ACCESS_TOKEN = ""
 
 
 def get_page_access_token():
@@ -8,3 +9,7 @@ def get_page_access_token():
 
 def get_verify_token():
     return _VERIFY_TOKEN
+
+
+def get_api_ai_client_access_token():
+    return _API_AI_CLIENT_ACCESS_TOKEN
