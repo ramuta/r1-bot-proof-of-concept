1b58d89ff072de9bf7fcd0036a1cd10a9d63fefc
Matej Ramuta <matej.ramuta@gmail.com>
2016-06-07 11:23:16 +0200

    API.ai connection added

--- main.py
+++ main.py
@@ -2,25 +2,44 @@ import webapp2
 import json
 import logging
 from google.appengine.api import urlfetch
-from secret import get_verify_token, get_page_access_token
+from secret import get_verify_token, get_page_access_token, get_api_ai_client_access_token
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        return self.response.write('Hello world!')
+        return self.response.write('Hello world! I am a bot :)')
 
 
 def send_to_messenger(sender_id, message):
-    respond = "User with ID %s said: %s" % (sender_id, message)
     json_data = {
         "recipient": {"id": sender_id},
-        "message": {"text": respond}
+        "message": {"text": message}
     }
     url = "https://graph.facebook.com/v2.6/me/messages?access_token=" + get_page_access_token()
     logging.info(url)
     data = json.dumps(json_data)
     result = urlfetch.fetch(url=url, payload=data, method=urlfetch.POST, headers={"Content-Type": "application/json"})
     logging.info(str(result.content))
+
+
+def send_to_api_ai(message, sender_id):
+    auth = get_api_ai_client_access_token()
+    url = "https://api.api.ai/v1/query"
+    params = {"query": message, "v": "20150910", "sessionId": "1234567890", "lang": "en"}
+    logging.info(str(params))
+    result = urlfetch.fetch(url=url,
+                            payload=json.dumps(params),
+                            method=urlfetch.POST,
+                            headers={'Content-Type': 'application/json; charset=utf-8',
+                                     "Authorization": "Bearer %s" % auth})
+    logging.info(str(result.content))
+    bot_response = json.loads(result.content)
+    speech = bot_response["result"]["speech"]
+    logging.info(speech)
+    if not speech:
+        send_to_messenger(sender_id, "Sorry, didn't understand what you mean... Ask me to tell you a joke :)")
+    else:
+        send_to_messenger(sender_id, speech)
 
 
 class WebhookHandler(webapp2.RequestHandler):
@@ -38,7 +57,7 @@ class WebhookHandler(webapp2.RequestHandler):
         try:
             message = messaging_events["entry"][0]["messaging"][0]["message"]["text"]
             logging.info("%s: %s" % (sender_id, message))
-            send_to_messenger(sender_id, message)
+            send_to_api_ai(message, sender_id)
             return self.response.write("success")
         except Exception, e:
             error = "A POST without 'message' field."
